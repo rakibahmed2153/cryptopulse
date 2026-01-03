9563b45f8bb87536d52eac3aa9694264de2c2c60
rakibahmed2153 <rakibahmed412245@gmail.com>
2026-01-03 23:59:15 +0100

    Data table design

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Image from "next/image";
+import DataTable from "@/components/DataTable";
 
 const Page = () => {
   return (
-      <div/>
+      <main className="main-container">
+        <section className="home-grid">
+          <div className="coin-overview">
+              <div className="header pt-2">
+                 <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png"
+                        alt="Bitscoin" width={56} height={56}/>
+                <div className="info">
+                  <p>Bitcoin / BTC</p>
+                  <h1>$89,113.00</h1>
+                </div>
+              </div>
+          </div>
+
+           <p>Trending Coins</p>
+            <DataTable />
+        </section>
+
+        <section className="w-full mt-7 space-y-4">
+          <p>Categories</p>
+        </section>
+      </main>
   )
 }
 
