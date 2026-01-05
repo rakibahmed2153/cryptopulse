780b2eb5e2e8ba8cd35fcf515b2735a45b4923d3
rakibahmed2153 <rakibahmed412245@gmail.com>
2026-01-05 01:59:32 +0100

    test with dummy data

--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -7,10 +7,10 @@ import {cn} from "@/lib/utils";
 
 const DataTable = <T,> ({columns, data, rowKey, tableClassName, headerRowClassName,
                           headerCellClassName, bodyRowClassName,
-                          bodyCellClassName}: DataTableProps<T>) => {
+                          bodyCellClassName, headerClassName}: DataTableProps<T>) => {
     return (
         <Table className={cn('custom-scrollbar', tableClassName)}>
-            <TableHeader className={headerCellClassName}>
+            <TableHeader className={headerClassName}>
                 <TableRow className={cn('hover:bg-transparent', headerRowClassName)}>
                     {columns.map((column, index) => (
                         <TableHead key={index} className={cn('bg-dark-400 text-purple-100 py-4 first:pl-5 last: pr-5')}>{column.header}</TableHead>
@@ -18,18 +18,16 @@ const DataTable = <T,> ({columns, data, rowKey, tableClassName, headerRowClassNa
                 </TableRow>
             </TableHeader>
             <TableBody>
-                <TableRow>
-                    {data.map((row, rowIndex) => (
-                        <TableRow key={rowKey(row, rowIndex)} className={cn('overflow-hidden rounded-lg border-b' +
-                            'border-purple-100/5 hover:bg-dark-400/30 relative', bodyRowClassName)}>
-                            {columns.map((column, indexColumn) => (
-                                <TableCell key={indexColumn} className={cn('py-4 first:pl-5 last:pr-5', headerCellClassName)} >{column.header}
-                                    {column.cell(row, rowIndex)}
-                                </TableCell>
-                            ))}
-                        </TableRow>
+                {data.map((row, rowIndex) => (
+                <TableRow key={rowKey(row, rowIndex)} className={cn('overflow-hidden rounded-lg border-b' +
+                    'border-purple-100/5 hover:bg-dark-400/30 relative', bodyRowClassName)}>
+                    {columns.map((column, indexColumn) => (
+                        <TableCell key={indexColumn} className={cn('py-4 first:pl-5 last:pr-5')}>
+                            {column.cell(row, rowIndex)}
+                        </TableCell>
                     ))}
                 </TableRow>
+                ))}
             </TableBody>
         </Table>
     );
